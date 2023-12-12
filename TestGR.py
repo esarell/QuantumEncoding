1a1fb8c8e9879d4d01976ed6ef25ce14bd53e147
Lizzie Sarell <lizziesarell@outlook.com>
2023-12-12 14:11:06 +0000

    First basic test

---
+++ TestGR.py
@@ -0,0 +1,21 @@
+import ProbabilityDistribution as PD
+import GroverRudolph as GR
+
+def TestSimpleDistribution():
+    '''
+    Testing the GR algorithm for a straight line
+    Seeing if this can be encoded into the amplitude
+    '''
+    distribution = PD.straightLine(3)
+    print(distribution)
+    amplitude = GR.Grover_Rudolph_func(3,distribution)
+    print(amplitude)
+    if amplitude == distribution:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    print("Simple Test:"+str(TestSimpleDistribution()))
+    
