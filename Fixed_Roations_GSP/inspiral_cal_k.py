28d5e57f01f318d87962987c29d957239d31a83f
Lizzie Sarell <lizziesarell@outlook.com>
2024-12-05 16:02:01 +0000

    Cleaned up my inspiral caculations, made main function to loop through sections of the distribution

--- Fixed_Roations_GSP/inspiral_cal_k.py
+++ Fixed_Roations_GSP/inspiral_cal_k.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import math
+'''This file calculates the relevant \eta values and corresponding k values
+for the inspiral of our gravitational wave approx f^{7/6} '''
 
 def plot_n():
     value = 0
@@ -52,7 +54,7 @@ def plot_n_movingfmin():
 def cal_k(data):
     xpoints =data[0]
     ypoints =data[1]
-    error_rate =0.999
+    error_rate =0.9999
     k_values =[]
     first_log =math.log(error_rate,10)
     for n in ypoints:
@@ -69,6 +71,10 @@ def cal_k(data):
     plt.show()   
 
 def optimal_fsplit():
+    """
+    Returns: a 2 dimensional array containg two sets of coordinates
+    one for the lower point one for our upper points
+    """
     lower_bound = 91
     upper_bound = 171
     fmin =40
@@ -117,76 +123,43 @@ def label_gate_concept():
     
 
 def calculate_n_k(fmin,fsplit,error_rate):
-    print("error_rate:",error_rate)
+    """
+    Args:
+    fmin: the minumu frequency of our section
+    fsplit: the max frequency of our section
+    error_rate: what is the maximum error we are willing to induce
+
+    returns:
+    nk: list contaiing eta, and its corresponding k
+    """
+    #calculate /eta based off Theorem 1 (Marin-Sanchez, et al. 2023)
     lower_deltaf = fsplit-fmin
     lower_bel_info = ((0*lower_deltaf)+fmin)**2
     lower_info = (7/3)*((lower_deltaf**2)/lower_bel_info)
     print("n:",lower_info)
+
+    #Uses eta to calcualte our level (k) where we can switch to fixed bounds
     first_log =math.log(error_rate,10)
     second_log =math.log((4**-9 - ((96/lower_info**2)*first_log)),2  )
     k = -0.5 * second_log
     print("k:",k)
+    nk = [lower_info,k]
+    return nk
 
 
-
-
-
-#result=optimal_fsplit()
-#cal_k(result[0])
-#cal_k(result[1])
-print("test 1:no controls")
-calculate_n_k(40,350,0.9999)
-print("\ntest 2: 1 controls")
-calculate_n_k(40,195,0.9999)
-calculate_n_k(195,350,0.9999)
-#calculate_n_k(155,253,0.9997)
-#calculate_n_k(98,155,0.9997)
-#calculate_n_k(253,350,0.9997)
-print("\ntest 3: 2 controls")
-calculate_n_k(40,117.5,0.9999)
-calculate_n_k(117.5,195,0.9999)
-calculate_n_k(195,272.5,0.9999)
-calculate_n_k(272.5,350,0.9999)
-
-print("\ntest 4: 3 controls")
-calculate_n_k(40,78.75,0.9999)
-calculate_n_k(78.75,117.5,0.9999)
-calculate_n_k(117.5,156.25,0.9999)
-calculate_n_k(156.25,195,0.9999)
-calculate_n_k(195,233.75,0.9999)
-calculate_n_k(233.75,272.5,0.9999)
-calculate_n_k(272.5,311.25,0.9999)
-calculate_n_k(311.25,350,0.9999)
-
-print("\ntest 5: 4 controls\njust for first bits")
-calculate_n_k(40,59.375,0.9999)
-calculate_n_k(59.375,78.75,0.9999)
-calculate_n_k(78.75,98.125,0.9999)
-calculate_n_k(98.125,117.5,0.9999)
-
-print("Conclusion: dont need more than 4 controls")
-#calculate_n_k(40,69,0.99999)
-#calculate_n_k(69,98,0.99999)
-#calculate_n_k(98,127,0.99999)
-#calculate_n_k(127,155,0.99999)
-#calculate_n_k(155,204,0.99999)
-#calculate_n_k(204,253,0.99999)
-#calculate_n_k(253,302,0.99999)
-#calculate_n_k(302,350,0.99999)
-
-#calculate_n_k(40,55,0.9999999)
-#calculate_n_k(55,69,0.9999999)
-#calculate_n_k(69,84,0.9999999)
-#calculate_n_k(84,98,0.9999999)
-#calculate_n_k(98,113,0.9999999)
-#calculate_n_k(113,127,0.9999999)
-#calculate_n_k(127,141,0.9999999)
-#calculate_n_k(141,155,0.9999999)
-
-
-
-
-#result_n=plot_n()
-#cal_k(result_n)
-#result_fmin=plot_n_movingfmin()
-#cal_k(result_fmin)
+if __name__ == "__main__":
+    #Splits up my distribution for n levels 
+    #Calculates a eta and corresponding k value
+    err = 0.9999
+    n = 3
+    fmin =40
+    fmax = 350
+    print("Testing for err:",err)
+    for m in range(n):
+        print("level:",m)
+        temp_fmin = fmin
+        increment = (fmax-fmin)/2**m
+        for j in range(2**m):
+            end = temp_fmin +increment
+            calculate_n_k(temp_fmin,end,err)
+            temp_fmin = end
