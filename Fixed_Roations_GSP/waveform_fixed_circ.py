6065aa60de460451c64c96e594ded9b4b0e78bbb
Lizzie Sarell <lizziesarell@outlook.com>
2025-04-25 13:11:49 +0100

    Code to calculate new eta values for the waveform. This results in a new circuit which is then implement. Improves overall fidelity, however larger circuit size. Was an error in previous eta calculating

--- Fixed_Roations_GSP/waveform_fixed_circ.py
+++ Fixed_Roations_GSP/waveform_fixed_circ.py
@@ -7,6 +7,11 @@ import pickle
 
 #This is Roselyns code
 def GenBinaryStrings(length):
+    """
+
+    :param length:
+    :return:
+    """
     binary_strings = []
     max_num = 2 ** length
 
@@ -18,11 +23,23 @@ def GenBinaryStrings(length):
 
 #This is Roselyns code
 def ReverseString(stringlist):
+    """
+
+    :param stringlist:
+    :return:
+    """
     reversed_list = [string[::-1] for string in stringlist]
     return reversed_list
 
 #This is Roselyns code with my additions
 def General_State_Prep(rotations):
+    """
+
+    :param rotations:
+    :return:
+    """
+    print("rotations:")
+    print(rotations)
     index=1
     num_qubits=len(rotations).bit_length()
     #builds blank circuit
@@ -46,13 +63,17 @@ def General_State_Prep(rotations):
 
 
 def ThetaRotation(circ,qr,condition,qubit_no,theta,wrap =True):
-    '''Args:
-    circ: quantum circuit
-    qr: quantum register
-    condition:list of str what conditions should it have
-    qubit_no:which qubit
-    wrap: boolean turns indivdual gates into one big gate defualt True 
-    Returns: '''
+    """
+
+    :param circ: quantum circuit
+    :param qr: quantum register
+    :param condition: list of str what conditions should it have
+    :param qubit_no:
+    :param theta:
+    :param wrap: boolean turns indivdual gates into one big gate defualt True
+    :return:
+    """
+
     size_qr = len(qr)
     if wrap == True:
         qr =qt.QuantumRegister(size_qr,"qr")
@@ -60,7 +81,6 @@ def ThetaRotation(circ,qr,condition,qubit_no,theta,wrap =True):
     
     for count,i in enumerate(condition):
         start = len(i)
-        print("i",len(i))
         rotation_gate = RYGate(theta[count]).control(len(i),ctrl_state=i)
         #[*qr[num_qubits-i-1:][::-1]
         #The 8 here is currently hard coded for n=9 qubits, this could be easily changed by passing through n as a parameter
@@ -75,6 +95,11 @@ def ThetaRotation(circ,qr,condition,qubit_no,theta,wrap =True):
     return circ
 
 def prob_normalised(data):
+    """
+
+    :param data:
+    :return:
+    """
     result=[]
     for x in data:
         square = x*x
@@ -83,6 +108,11 @@ def prob_normalised(data):
     return result
 
 def amp_normalised(data):
+    """
+
+    :param data:
+    :return:
+    """
     result=[]
     for x in data:
         norm = np.sqrt(x/sum(data))
@@ -90,19 +120,21 @@ def amp_normalised(data):
     return result
 
 def waveform_amps():
+    """
+
+    :return:
+    """
     with open("waveform_amps.pkl", 'rb') as pickleFile:
         test = pickle.load(pickleFile)
-    print(test[0])
     return test
 
 def Fidelity(expected_amps,measured_amps):
-    '''
-    Args: 
-    expected_amps: list of the amplitudes from the discritsed function
-    measured_amps: list of amplitudes taken from the statevector of the quantum circuit
-    Returns:
-    the fidelity (float)
-    '''
+    """
+
+    :param expected_amps: list of the amplitudes from the discritsed function
+    :param measured_amps: list of amplitudes taken from the statevector of the quantum circuit
+    :return: the fidelity (float)
+    """
     current =0
     for count,i in enumerate(expected_amps):
         current = current+(i*measured_amps[count])
@@ -112,7 +144,12 @@ def Fidelity(expected_amps,measured_amps):
 
 
 def waveform_theta(frequency,m):
-    #[0.74928181,0.73045836,0.72835018,0.71932391,0.73274099,0.74580992,0.75401846]
+    """
+
+    :param frequency:
+    :param m:
+    :return:
+    """
     j=0
     #This is specifically for f^-7/6 (so p(i)is f^-7/3)
     #Will need changing for a more complicatated distribution
@@ -123,7 +160,7 @@ def waveform_theta(frequency,m):
     gsp_theta=[]
     for i in range(m):
         j=pow(2,i)
-        if i<3:
+        if i<4:
             for x in range(j):
                 start=int(x*pow(2,m-i))
                 mid = int((x+0.5)*pow(2,m-i))
@@ -134,79 +171,77 @@ def waveform_theta(frequency,m):
                 costheta = np.sqrt(costheta2)
                 theta = np.arccos(costheta) 
                 gsp_theta.append(theta*2)
-        elif i>=3:
-            #print("m:",i)
-            if i==3:
+                print()
+        elif i>=4:
+            if i == 4:
                 thetas.append(gsp_theta)
-                '''Starting points frequency: 40,78.75,117,195,272.5
+            #print("m:",i)
+            '''if i==3:
+                thetas.append(gsp_theta)
+                Starting points frequency: 40,78.75,117,195,272.5
                 #These indexs are hard coded for when n=9 would need to be changed for different points
-                #Also these directly correspond with the set up of the circuit so would also need to be changed if the circuit changes'''
-                index=[0,64,128,256,384]
+                #Also these directly correspond with the set up of the circuit so would also need to be changed if the circuit changes
+                index=[0,64,128,256,320,384]
                 #index=[0,4,8,12,16,24]
             else:
-                '''Set starting points for all the
+                Set starting points for all the
                 40, 59.375, 78.75, 117.5, 195, 272.5
-                0,32,64,128,256,384'''
+                0,32,64,128,256,384
                 index=[0,32,64,128,256,384]
-                #index=[0,2,4,8,16,24]
+                #index=[0,2,4,8,16,24]'''
+            index =[0,32,64,128,192,256,320,352,384,416,448]
             temp_theta =[]
             for current_index in index:
                 temp_start=current_index
-                print("start:",frequency[temp_start])
                 place = current_index/pow(2,m-i)
-                print("x:",place)
                 increment = int(pow(2,m)/pow(2,i))
                 mid =int((place+0.5)*pow(2,m-i))
                 end=int((place+1)*pow(2,m-i))
-                print("increment:",increment)
-                print("mid",mid)
-                print("end:",end)
                 upper = sum(amps[current_index:mid])
                 lesser =sum(amps[current_index:end])
                 costheta2 = upper/lesser
                 costheta = np.sqrt(costheta2)
-                theta = np.arccos(costheta) 
-                print("theta:",theta)
+                theta = np.arccos(costheta)
                 temp_theta.append(theta*2)
             thetas.append(temp_theta)
     if i<3:
         thetas.append(gsp_theta)
-    #print("theata:",thetas)
+    print("theata:",thetas)
     return thetas
 
 
-def Waveform_Fixed_Rots(n):
-    """Ashwins theta values
-    [0.69134909] 
- [0.75468131 0.498768  ] 
- [0.7304692  0.75837712 0.70859427 0.66545498] 
- [0.72251893 0.74890938 0.75837712 0.70859427 0.66545498] 
- [0.73581314 0.74934639 0.74890938 0.75837712 0.70859427 0.66545498] """
+
+def Waveform_Fixed_Rots(n,Plot=False):
+    """
+    Create a quantum circuit for the whole gravitational waveform
+    Waveform can be seen in the LinearSplineApprox.py file
+    :param n: number of qubits
+    :return:
+    """
     qr= qt.QuantumRegister(size=n,name='q')
     cla_reg =qt.ClassicalRegister(size=n,name="cla")
     circ = qt.QuantumCircuit(qr,cla_reg)
-    #basic = General_State_Prep([0.69134909,0.75468131,0.498768,0.7304692,0.75837712, 0.70859427, 0.66545498])
-        #Generate theta values for the GSP section
+
+    #Generate theta values for the GSP section
     #A list of frequency values evenly spaced, the amount is based on n
     frequency = np.linspace(40,350,num=pow(2,n),endpoint=False)
     angles = waveform_theta(frequency,n)
-    basic = General_State_Prep(angles[0])
+    GSP = General_State_Prep(angles[0])
     
-    circ.append(basic,qr[n-3:n])
+    circ.append(GSP,qr[n-4:n])
     circ.save_statevector(label='v1')
-    #This is for m=3
+    '''#This is for m=3
     #circ.save_statevector()
-    controls =["000","001","01","10","11"]
+    controls =["000","001","01","100","101","11"]
     m_3=ThetaRotation(circ,qr,controls,5,angles[1],True)
-    circ.append(m_3,[*qr])
+    circ.append(m_3,[*qr])'''
     #for the rest of the levels m>3
     circ.save_statevector(label='v2')
     
     for i in range((n-4)):
-        controls =["0000","0001","001","01","10","11"]
-        #theta_m4=[0.73581314,0.74934639,0.74890938,0.75837712,0.70859427,0.66545498]
-        #theta_m4=[[0.74066914,0.75075189,0.75726406,0.76502399,0.77230382,0.77575838],[0.75760343,0.76520445,0.76942786,0.77434713,0.77851473,0.78040074],[0.76978013,0.77445391,0.77707962,0.77963685,0.78186772,0.78285331],[0.77710115,0.77969497,0.78110797,0.78245585,0.78361015,0.78411397],[0.78111943,0.78248617,0.78321923,0.77277432,0.78449838,0.7847531]]
-        fixed=ThetaRotation(circ,qr,controls,8-(4+i),angles[i+2],True)
+        #controls =["0000","0001","001","01","10","11"]
+        controls =["0000","0001","001","010","011","100","1010","1011","1100","1101","111"]
+        fixed=ThetaRotation(circ,qr,controls,8-(4+i),angles[i+1],True)
         circ.append(fixed,[*qr])
     circ.save_statevector(label='v3')
     backend = QasmSimulator()
@@ -218,25 +253,42 @@ def Waveform_Fixed_Rots(n):
     statevector3=result.data(0)['v3']
     circ.decompose().draw("mpl",fold=-1)
     plt.show()
-    print(statevector)
-    print(statevector2)
-    print(statevector3)
 
     #amps = waveform_amps()
     temp_amps= waveform_amps()
     amps=amp_normalised(temp_amps)
     #amps_7_6 = amplitudes_7_over_6(frequency)
-    plt.plot(frequency,np.sqrt(statevector3.probabilities()),color='k',label='Statevector')
-    plt.plot(frequency,amps,color='r',label='Waveform')
-    #plt.plot(frequency,amps_7_6,color='b',label='Amps -7/6')
-    plt.legend()
-    plt.xlabel('f (Hz)')
-    plt.ylabel('A')
-    plt.show()
-    print("Fidelity: ",Fidelity(amps,np.sqrt(statevector3.probabilities())))
 
+    if Plot:
+        rc_results = np.sqrt(statevector3.probabilities())
+        result_fig = plt.figure()
+        result_fig.set_figwidth(8)
+        result_fig.set_figheight(6)
+        plt.plot(frequency,rc_results,color='k',label='Statevector',ms = 2)
+        plt.plot(frequency,amps,color='r',label='Amps -7/6',linestyle='dashed')
+        plt.legend()
+        plt.xlabel('f (Hz)')
+        plt.ylabel('A')
+        result_fig.savefig("../Images/Waveform_Amplitudes.png",dpi=1000,bbox_inches='tight')
+        plt.show()
+
+        #Plot the box plot to see the errors
+        difference = rc_results - amps
+        fig = plt.figure()
+        fig.set_figwidth(8)
+        fig.set_figheight(2)
+        plt.boxplot(abs(difference),vert=False,widths=1)
+        plt.ylim(0,2)
+        plt.yticks([])
+        plt.xlabel("Error")
+        #plt.title("Boxplot showing the errors between the datapoints encoded by the reduced circuit\n and the intended amplitudes.")
+        fig.savefig('../Images/Waveform_boxplot.png', dpi=1000,bbox_inches='tight')
+        plt.show()
+
+
+    print("Fidelity: ",Fidelity(amps,np.sqrt(statevector3.probabilities())))
+    print(dict(circ.decompose().decompose().decompose().decompose().count_ops()))
 
 
 if __name__ == "__main__":
-    waveform_amps()
-    Waveform_Fixed_Rots(9)
+    Waveform_Fixed_Rots(9,Plot=True)
--- Fixed_Roations_GSP/waveform_fixed_rotation_cal.py
+++ Fixed_Roations_GSP/waveform_fixed_rotation_cal.py
@@ -15,18 +15,6 @@ def straightline(point1,point2):
     c = point1[1]- m*point1[0]
     return [m,c]
 
-def inspiral_point():
-    no =33
-    fmin =40
-    fmax =350
-    delta = (fmax-fmin)/no
-    data=[]
-    for i in range(34):
-        amplitude =inspiral_fun(fmin)
-        result=[fmin,amplitude]
-        data.append(result)
-        fmin=fmin+delta
-    return data
 
 
 
@@ -43,7 +31,7 @@ def rescale_f(current_Freq,fmin,fmax):
     new = (current_Freq - fmin) /(fmax-fmin)
     return new
 
-def numerical_second_devirative(datapoint,fmin=40,fmax=350,Plot=True):
+def numerical_second_devirative(datapoint,fmin,fmax,Plot=True):
     """Get the numerical second log devirative  """
     amps =[]
     x_list=[]
@@ -53,11 +41,14 @@ def numerical_second_devirative(datapoint,fmin=40,fmax=350,Plot=True):
     count = 1
     for i in datapoint:
         """Rescale the fucntion, then find the log of the function squared"""
+
         x = rescale_f(i[0],fmin,fmax)
-        x_list.append(x)
+
+        x_list.append(i[0])
         amps.append(i[1])
         #log of the function squared
         y_list.append(math.log(math.pow(i[1],2)))
+
 
     for count,value in enumerate(y_list):
         if count+1!=len(datapoint):
@@ -96,14 +87,14 @@ def numerical_second_devirative(datapoint,fmin=40,fmax=350,Plot=True):
     eta = max(second_devirative)
     if eta == 0.0:
         eta = second_devirative[0]
-    print("eta:",eta)
+
 
     second_devirative.append(stright_line_info[0])
     second_devirative.append(stright_line_info[0])
 
     return [eta,second_devirative]
 
-def numerical_512(n,err,no_datapoint,datafile):
+'''def numerical_512(n,err,no_datapoint,datafile):
     """
     For 512 datapoints, we calculate eta and corresponding k values for each section
     Note that this code doesn't work for the full waveform as it is not 2nd order differentable.
@@ -111,9 +102,9 @@ def numerical_512(n,err,no_datapoint,datafile):
     n: number of qubits/level we want to split up to
     err: the error we are willing to induce
     """
-    ''' for 350 datapoints
+     for 350 datapoints
     with open("datapoints", "rb") as fp:   # Unpickling
-        datapoints = pickle.load(fp)'''
+        datapoints = pickle.load(fp)
     with open(datafile, 'rb') as pickleFile:
         datapoints = pickle.load(pickleFile)
     fmin =40
@@ -134,7 +125,8 @@ def numerical_512(n,err,no_datapoint,datafile):
             k=calculate_k(eta,err)
             print("k:",k)
             temp_fmin = freq_end
-            temp_data_min = data_end
+            temp_data_min = data_end'''
+
 
 def find_split(split,data):
     """
@@ -143,11 +135,15 @@ def find_split(split,data):
     :param data: a list of all the datapoints
     :return: list index
     """
-    print("split",data[0])
+    #print("split",data[0])
     for count,i in enumerate(data):
-        if i[0]>split:
-            print(i[0])
+        if count ==0 and i[0]>split:
+            return [None,None]
+        elif i[0]>split:
+            #print(i[0])
             return [count,i[0]]
+
+    return [None,None]
 
 
 '''def derivative(data,Plot=True):
@@ -180,57 +176,88 @@ def find_split(split,data):
     eta = np.abs(div_2)
     return np.max(eta)'''
 
-def divirative_section(datafile,splits,err):
+def divirative_section(current_data,splits,err,fmin,fmax,Plot=True):
     """
     Do the second divitative of 3 different sections, then piece them together
-    :param datafile:
+    :param current_data:
     :param splits:
     :return: Divirative of the wholewaveform
     """
     prev_bound =0
-    fmin =40
-    with open(datafile, 'rb') as pickleFile:
-        datapoints = pickle.load(pickleFile)
-    print(datapoints[0])
     bounds=[]
     results=[]
     x_results=[]
     y_vals=[]
     log_val=[]
-    '''eta = derivative(datapoints,Plot=True)
-    print("eta:",eta)
-    k=calculate_k(eta,err)
-    print("k:",k)'''
-    for count,i in enumerate(splits):
-        bound = find_split(i,datapoints)
-        bounds.append(bound)
 
-    for count,j in enumerate(datapoints):
-        x = rescale_f(j[0],40,350)
-        datapoints[count][0]=x
+    for count,i in enumerate(splits):
+        bound = find_split(i,current_data[:])
+        if bound[0] == None:
+            print("No Bound")
+        else:
+            bounds.append(bound)
+    for count,j in enumerate(current_data[:]):
+        x = rescale_f(j[0],fmin,fmax)
+        current_data[count][0]=x
         x_results.append(x)
         y_vals.append(j[1])
         log_val.append(math.log(math.pow(j[1],2)))
 
     for z in bounds:
-        temp_data = datapoints[prev_bound:z[0]]
-        print("prev:",prev_bound)
-        results = results +numerical_second_devirative(temp_data,Plot=True)[1]
+        temp_data = current_data[prev_bound:z[0]]
+        #print("prev:",prev_bound)
+        results = results +numerical_second_devirative(temp_data,fmin=fmin,fmax=fmax,Plot=False)[1]
         prev_bound = z[0]
-    temp_data= datapoints[prev_bound:]
-    results = results + numerical_second_devirative(temp_data,Plot=True)[1]
+    temp_data= current_data[prev_bound:]
+    results = results + numerical_second_devirative(temp_data,fmin=fmin,fmax=fmax,Plot=False)[1]
 
-    print("here")
-    plt.plot(x_results, y_vals,label="Waveform")
-    plt.plot(x_results, log_val,label="log(f^2(x))",linestyle='-')
-    plt.plot(x_results, results,label="2nd derivative",linestyle='-.')
-    plt.xlabel("f (Hz)")
-    plt.ylabel("Amp")
-    plt.legend()
-    plt.show()
+    if Plot == True:
+        plt.plot(x_results, y_vals,label="Waveform")
+        plt.plot(x_results, log_val,label="log(f^2(x))",linestyle='-')
+        plt.plot(x_results, results,label="2nd derivative",linestyle='-.')
+        plt.xlabel("f (Hz)")
+        plt.ylabel("Amp")
+        plt.legend()
+        plt.show()
+    abs_result =np.abs(np.array(results))
+    eta = max(abs_result)
+    print("eta:",eta)
+    k=calculate_k(eta,err)
+    print("k:",k)
+
+def waveform_derivative_cal(datafile,n,err):
+
+    fmin =40
+    fmax =350
+    data_min =0
+    no_datapoint = 512
+    print("Testing for err:",err)
+    splits= [43.728557756818006,243.75275131383347]
+    #divirative_section(datapoints,splits,0.99999,40,350,True)
+
+    for m in range(n):
+        with open(datafile, 'rb') as pickleFile:
+            datapoints = pickle.load(pickleFile)
+        print("level:",m)
+        temp_fmin = fmin
+        temp_data_min = data_min
+        freq_increment = (fmax-fmin)/2**m
+        data_increment = no_datapoint/2**m
+        for j in range(2**m):
+            print("j: ",j)
+            freq_end = temp_fmin +freq_increment
+            data_end = temp_data_min +data_increment
+            divirative_section(datapoints[int(temp_data_min):int(data_end)],splits,err=err,fmin= temp_fmin,fmax= freq_end,Plot=True)
+            #eta=numerical_second_devirative(datapoints[int(temp_data_min):int(data_end)],temp_fmin,freq_end)[0]
+
+            temp_fmin = freq_end
+            temp_data_min = data_end
+
+
 
 if __name__ == "__main__":
     #insprial_data =inspiral_point()
     datafile="waveform_data.pkl"
     #divirative_section(datafile,[43.728557756818006,243.75275131383347],0.99999)
-    numerical_512(3,0.9999,512,datafile)
+    waveform_derivative_cal(datafile,5,0.9999)
+    #numerical_512(3,0.9999,512,datafile)
